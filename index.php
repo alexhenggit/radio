fda9c5752d2d9a52165b563d75eda2077b7aae2b
alexhenggit <49366695+alexhenggit@users.noreply.github.com>
2019-04-07 22:12:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,17 +11,17 @@
   <title>Radio Emas</title>
 
   <!-- Font Awesome Icons -->
-  <link href="./RadioApp/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://github.com/alexhenggit/radio/blob/master/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
   <!-- Plugin CSS -->
-  <link href="./RadioApp/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="https://github.com/alexhenggit/radio/blob/master/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
   <!-- Theme CSS - Includes Bootstrap -->
-  <link href="./RadioApp/css/creative.min.css" rel="stylesheet">
+  <link href="https://github.com/alexhenggit/radio/blob/master/css/creative.min.css" rel="stylesheet">
 
 
 
@@ -392,15 +392,15 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="./RadioApp/vendor/jquery/jquery.min.js"></script>
-  <script src="./RadioApp/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://github.com/alexhenggit/radio/blob/master/vendor/jquery/jquery.min.js"></script>
+  <script src="https://github.com/alexhenggit/radio/blob/master/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="./RadioApp/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="./RadioApp/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <script src="https://github.com/alexhenggit/radio/blob/master/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="https://github.com/alexhenggit/radio/blob/master/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="./RadioApp/js/creative.min.js"></script>
+  <script src="https://github.com/alexhenggit/radio/blob/master/js/creative.min.js"></script>
 
 
 </body>
